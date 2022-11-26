c67937ef86d1bc5df655fad0e478f26552e6809b
berquerant <sin@sin-pro.local>
2022-11-27 00:36:53 +0900

    go-wc: Use Scanner

--- go-wc/main.go
+++ go-wc/main.go
@@ -2,56 +2,27 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
-	"io"
 	"os"
-	"unicode"
+	"strings"
 )
 
 func main() {
 	var (
-		reader = bufio.NewReader(os.Stdin)
+		scanner = bufio.NewScanner(os.Stdin)
 
 		line  int
 		word  int
 		bytes int
-
-		prevIsSpace bool
-		isTail      bool
-		metChar     bool
 	)
 
-	for {
-		r, size, err := reader.ReadRune()
-		isEOF := errors.Is(err, io.EOF)
-		if !isEOF && err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-
-		bytes += size
-		isSpace := unicode.IsSpace(r)
-
-		switch {
-		case isSpace:
-			if r == '\n' {
-				line++
+	for scanner.Scan() {
+		line++
+		bytes += len(scanner.Bytes()) + 1 // newline
+		for _, x := range strings.Split(scanner.Text(), " ") {
+			if x != "" {
+				word++
 			}
-		case prevIsSpace && isTail && metChar:
-			// space non-space
-			// and have encountered a char except space before the rune
-			word++
-		}
-
-		if isEOF {
-			break
-		}
-
-		prevIsSpace = isSpace
-		isTail = true
-		if !metChar && !isSpace {
-			metChar = true
 		}
 	}
 
